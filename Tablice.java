44457d20f86faa6f0e2b59ea758098f61c41c69c
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-02-23 09:43:35 +0100

    final

--- Tablice.java
+++ Tablice.java
@@ -1,20 +1,30 @@
 import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
 
 class Tablice{
     public static void main(String[] args){
 		
-		
+		String imienazwisko[] = new String[10];
 		int oceny[] = new int[10];
 		int i;
 		for(i=0; i<10; i++){
 			oceny[i] = (int)(Math.random()*6+1);
         }
-		
-		
+		i = 0;
+		try{
+            Scanner sc = new Scanner(new File("imienazwisko.txt"));
+            while(sc.hasNext()){
+                imienazwisko[i] = sc.nextLine();
+				i++;
+            }
+        }catch (FileNotFoundException ex){
+            System.out.println("ERROR -> "+ex.toString());
+        }
 		
 		
 		for(i=0; i<10; i++){
-			System.out.println(oceny[i]);
+			System.out.println(imienazwisko[i] +" "+  oceny[i]);
         }
     }
 }
