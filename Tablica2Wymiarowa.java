0165b64c90c40131e9b2996d5be63272e9d8ab7c
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-03-01 09:28:54 +0100

    Przeniesienie do metod

--- Tablica2Wymiarowa.java
+++ Tablica2Wymiarowa.java
@@ -2,12 +2,10 @@
 
 class Tablica2Wymiarowa{
 	public static void main(String [] a){
-		int [] tab = new int[5];
-		tab [0] = 40;
-
-		//for(int i=0;i<tab.length;i++){
-        //    System.out.println(tab[i]);
-        //}
+		tab2dFirst();
+		przypomnienietablicjednowymiarowych();
+	}	
+	private static void tab2dFirst(){
 		int [][] tab2d = new int[5][2];
 		tab2d[0][1] = 50;
 		System.out.println(tab2d.length);
@@ -20,5 +18,15 @@ class Tablica2Wymiarowa{
 		System.out.println("Koniec pętli j");
 		}
 		System.out.println("Koniec pętli i");
+	
 	}
+	private static void przypomnienietablicjednowymiarowych(){
+		int [] tab = new int[5];
+		tab [0] = 40;
+
+		for(int i=0;i<tab.length;i++){
+            System.out.println(tab[i]);
+        }
+	}
+	
 }
