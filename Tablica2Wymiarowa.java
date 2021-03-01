3097af62f6ebf1c7b82a5e41b68734ac763e2f95
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-03-01 09:25:56 +0100

    Wyświetlanie tablicy dwuwymairowej

--- Tablica2Wymiarowa.java
+++ Tablica2Wymiarowa.java
@@ -4,6 +4,21 @@ class Tablica2Wymiarowa{
 	public static void main(String [] a){
 		int [] tab = new int[5];
 		tab [0] = 40;
-		tab [1] = "Jeden";
+
+		//for(int i=0;i<tab.length;i++){
+        //    System.out.println(tab[i]);
+        //}
+		int [][] tab2d = new int[5][2];
+		tab2d[0][1] = 50;
+		System.out.println(tab2d.length);
+		System.out.println(tab2d.length*tab2d[0].length);
+		
+		for(int i=0;i<tab.length;i++){
+			for(int j=0;j<tab.length;j++){
+            System.out.println(tab2d[i][j]);
+			}
+		System.out.println("Koniec pętli j");
+		}
+		System.out.println("Koniec pętli i");
 	}
 }
