b8e76c7b7903e0a1f6f6589684f4b7df930d71c2
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-03-02 09:54:38 +0100

    Zadanie 1

---
+++ 02.03.2021/KamilOciepa.java
@@ -0,0 +1,17 @@
+
+
+class KamilOciepa{
+	public static void main(String [] a){
+		int [][] tab2d = new int [2][2];
+		tab2d [0][0] = 2;
+		tab2d [0][1] = 1;
+		tab2d [1][0] = 3;
+		tab2d [1][1] = 7;
+		for(int i=0;i<tab2d.length;i++){
+			for(int j=0;j<tab2d[i].length;j++){
+            System.out.println(tab2d[i][j]);
+			}
+		}
+	}	
+
+}
