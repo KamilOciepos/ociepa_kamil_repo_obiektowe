06f28f9aeb62f97481ec8b0ab93ddff21cd088f3
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-03-02 10:30:54 +0100

    Tabliczka Mnozenia

--- 02.03.2021/KamilOciepa.java
+++ 02.03.2021/KamilOciepa.java
@@ -3,9 +3,9 @@
 class KamilOciepa{
 	public static void main(String [] a){
 		
-		
+		TabliczkaMnozenia();
 	}
-	private static void tablica2x2{
+	private static void tablica2x2(){
 		int [][] tab2d = new int [2][2];
 		tab2d [0][0] = 1;
 		tab2d [0][1] = 2;
@@ -18,4 +18,18 @@ class KamilOciepa{
 		}
 	
     } 
+	private static void TabliczkaMnozenia(){
+		int [][] tab2d = new int [10][10];
+		for(int i=0;i<tab2d.length;i++){
+			for(int j=0;j<tab2d[i].length;j++){
+				tab2d[i][j] = (i+1)*(j+1);
+			}
+		}
+		for(int i=0;i<tab2d.length;i++){
+			for(int j=0;j<tab2d[i].length;j++){
+				System.out.print(tab2d[i][j]+"\t");
+			}
+			System.out.println();
+		}
+	}
 }
