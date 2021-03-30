99716fb714b5d4cf73ffba8451ca0ce0aa4ee26f
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-03-30 09:24:02 +0200

    zadanie 1

---
+++ cwiczenia 1.9.1/CwiczenieObiektowe_1.java
@@ -0,0 +1,9 @@
+
+class CwiczenieObiektowe_1{
+	
+	public static void main(String[] args){
+		MojeDane md = new MojeDane("Kamil","Ociepa","2PTN");
+		
+		
+	}
+}
--- cwiczenia 1.9.1/MojeDane.java
+++ cwiczenia 1.9.1/MojeDane.java
@@ -1,5 +1,16 @@
 
 class MojeDane{
 	
+	public String imie ="Kamil";
+	public String nazwisko ="Ociepa";
+	public String klasa ="2PTN";
+	
+	public MojeDane(String imie,String nazwisko,String klasa){
 		
+		this.imie = imie;
+		this.nazwisko = nazwisko;
+		this.klasa = klasa;
+		System.out.println(imie+" "+nazwisko+" "+klasa);
+	}
+	
 }
