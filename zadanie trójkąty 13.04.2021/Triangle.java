d8af9bf286087dc7fe35876dc64ec820e1062e49
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-04-13 09:52:09 +0200

    trojkat rownoboczny

--- zadanie trójkąty 13.04.2021/Triangle.java
+++ zadanie trójkąty 13.04.2021/Triangle.java
@@ -1,6 +1,12 @@
 class Triangle{
 	
+	public Triangle(int wysokoc,int podstawa){
+		System.out.println(wysokosc*podstawa/2);
+	}
 	
+	public EquilateralTriangle(int podstawa){
+		System.out.println(((podstawa*podstawa)*sqrt(3))/4
+	}
 	
 	
 }
