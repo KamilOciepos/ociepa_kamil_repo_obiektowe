91e8dc5785ee71cdf8f6a9d2c6408a4aad9fd073
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-04-20 08:53:20 +0200

    lekcja (zapomnialem commit)

---
+++ lekcja 19.04.2021/ArrayList/DaneOsobowe.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+class DaneOsobowe{
+
+    private String name = "";
+	private String lastname= "";
+	private int age = 0;
+	private int height = 0;
+	private int weight = 0;
+	
+	public DaneOsobowe(String name,String lastname,int age,int height,int weight){
+		this.name = name;
+		this.lastname = lastname;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+			
+			
+	}
+}
