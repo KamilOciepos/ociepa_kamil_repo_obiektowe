ee83fba097f175f382d41c426fa65015bd163b35
KamilOciepos <77607492+KamilOciepos@users.noreply.github.com>
2021-04-27 10:25:00 +0200

    commit tylko ze nie wiem czy wszystko jest
    troche sie pogubilem

--- 26.04.2021/ReadFromKeyboard.java
+++ 26.04.2021/ReadFromKeyboard.java
@@ -2,22 +2,20 @@ import java.util.Scanner;
 
 class ReadFromKeyboard{
 	public ToDo readOneToDo(){
-		
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Podaj tytuł: ");
 		String title = sc.nextLine();
 		System.out.println("Podaj treść notatki: ");
 		String content = sc.nextLine();
-		System.out.println("Podaj autor lub naciśnij ENTER jeśli nie chcesz podawać: ");
+		System.out.println("Podaj autora lub naciśnij ENTER: ");
 		String author = sc.nextLine();
 		
-		
-		if(author.length()<0){
-			return new ToDo(title,content);	
-		}else{
-			return new ToDo(tile,content,author);
+		if (author.length()<1){
+			return new ToDo(title,content);
+		} else {
+			return new ToDo(title,content,author);
 		}
+
 	}
-	
-	
 }
--- 26.04.2021/ToDo.java
+++ 26.04.2021/ToDo.java
@@ -1,28 +1,44 @@
-import java.time.LocalDateTime;
-
-class ToDo{
+class ToDo {
 	private String title = "";
 	private String content = "";
 	private String author = "Anonymous";
 	private LocalDateTime add_date = LocalDateTime.now();
 	
-	
+	//1 Constructor author unknown
 	public ToDo(String title, String content){
 		this.title = title;
 		this.content = content;
 	}
 	
-	public ToDo(String title,String content,String author){
+	//2 Constructor 
+	public ToDo(String title, String content, String author){
 		this.title = title;
 		this.content = content;
-		this.author = author;	
+		this.author = author;
+	}
+	
+	public String getContent(){
+		return content;
+	}
+	
+	public LocalDateTime getAdd_Date(){
+		return add_date;
+	}
+	
+	public String getAuthor(){
+		return author;
+	}
+	
+	public String getTitle(){
+		return title;
 	}
 	
 	@Override
-	
 	public String toString(){
 		return "Title: "+title+"\n"
 			+"Content: "+content+"\n"
 			+"Author: "+author+"\n"
-			+"Data: "+date+"\n";
+			+"Data: "+add_date+"\n";
+	}
+	
 }
