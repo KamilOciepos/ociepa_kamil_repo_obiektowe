466c07fc441299b35873d96e1beaf91e17cca8a5
KamilOciepos <kapeciu45@gmail.com>
2022-02-03 12:13:12 +0100

    komit kontrolny

--- blackjack/CardDraw.java
+++ blackjack/CardDraw.java
@@ -1,12 +1,16 @@
+import java.util.Random;
+
 class CardDraw {
     public static void carddraw() {
         String[] cardname = { "as", "król", "dama", "walet", "dziesięc", "dziewięć", "osiem", "siedem", "sześć", "pięć",
                 "cztery", "trzy", "dwa" };
-        int[] cardvalue = { 11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        String[] cardvalue = { "11", "10", "10", "10", "10", "9", "8", "7", "6", "5", "4", "3", "2" };
 
-        String wylosowana1 = cardname[];
-        String wylosowana2 = cardname[];
-        String krupier = cardname[];
+        int liczba1 = (int) (Math.random() * cardname.length + 1);
+        int liczba2 = (int) (Math.random() * cardname.length + 1);
+        int liczba3 = (int) (Math.random() * cardname.length + 1);
+        String wylosowana[][] = { { cardname[liczba1], cardvalue[liczba1] },
+                { cardname[liczba2], cardvalue[liczba2] }, { cardname[liczba3], cardvalue[liczba3] } };
 
     }
 }
---
+++ blackjack/GameLogic.java
@@ -0,0 +1,5 @@
+public class GameLogic {
+    public static void gamelogic() {
+
+    }
+}
