b09d6edb3629404247cadd803ad63846a48f7445
KamilOciepos <kapeciu45@gmail.com>
2022-02-03 11:27:07 +0100

    komit sprawdzajoncy 1

---
+++ blackjack/CardDraw.java
@@ -0,0 +1,12 @@
+class CardDraw {
+    public static void carddraw() {
+        String[] cardname = { "as", "król", "dama", "walet", "dziesięc", "dziewięć", "osiem", "siedem", "sześć", "pięć",
+                "cztery", "trzy", "dwa" };
+        int[] cardvalue = { 11, 10, 10, 10, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+
+        String wylosowana1 = cardname[];
+        String wylosowana2 = cardname[];
+        String krupier = cardname[];
+
+    }
+}
