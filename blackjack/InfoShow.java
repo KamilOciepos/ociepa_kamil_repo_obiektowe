758edb4d692255c4fbaeb591304a211e1a33af65
KamilOciepos <kapeciu45@gmail.com>
2022-02-07 09:32:43 +0100

    nie umie nic

--- blackjack/InfoShow.java
+++ blackjack/InfoShow.java
@@ -1,5 +1,21 @@
 public class InfoShow {
-    public InfoShow() {
 
+    public static void menuShow() {
+        System.out.println("Wybierz opcje z ponizszych:\n" +
+                "1 - Zacznij gre \n" +
+                "2 - Wyjdz");
+    }
+
+    public void optionsShow() {
+        System.out.println("Wybierz opcje z ponizszych \n" +
+                "[S] - Stand (czekanie) \n" +
+                "[H] - Hit (dobieranie) \n" +
+                "[P] - Split (podzielenie) \n" +
+                "[W] - Auto Win (wygrana) \n " +
+                "[F] - Poddanie");
+    }
+
+    public void missingOptions() {
+        System.out.println("Brak opcji");
     }
 }
--- blackjack/Main.java
+++ blackjack/Main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        InfoShow.menuShow();
+        String wybor = scan.nextLine();
 
+        switch (wybor) {
+            case "1";
+
+            case "2";
+        }
     }
 }
